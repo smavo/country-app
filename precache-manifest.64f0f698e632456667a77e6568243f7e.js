a51570ea07b830cc36a62b6909605eaeede2b95e
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-09-29 15:11:05 -0500

    Updates

---
+++ precache-manifest.64f0f698e632456667a77e6568243f7e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d427ccd5f1712432e7bfebf187d8b919",
+    "url": "/country-app/index.html"
+  },
+  {
+    "revision": "32b83c8f374f36aa7149",
+    "url": "/country-app/static/css/main.a1026fa7.chunk.css"
+  },
+  {
+    "revision": "e866751a9e25d456dc7c",
+    "url": "/country-app/static/js/2.5175048c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-app/static/js/2.5175048c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b83c8f374f36aa7149",
+    "url": "/country-app/static/js/main.5e88ca03.chunk.js"
+  },
+  {
+    "revision": "7768c05af8b87a749826",
+    "url": "/country-app/static/js/runtime-main.5b717548.js"
+  },
+  {
+    "revision": "3ef2b8e0dfd23ccae712e23d4effeb73",
+    "url": "/country-app/static/media/icomoon.3ef2b8e0.woff"
+  },
+  {
+    "revision": "5300ffcab9e7b0c354bec03f031f0586",
+    "url": "/country-app/static/media/icomoon.5300ffca.svg"
+  },
+  {
+    "revision": "544b321fb7a38bb33be460cb0c033abe",
+    "url": "/country-app/static/media/icomoon.544b321f.eot"
+  },
+  {
+    "revision": "be551178f6ff2ab22cc60a1c2922876b",
+    "url": "/country-app/static/media/icomoon.be551178.ttf"
+  }
+]);
